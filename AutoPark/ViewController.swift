bb895a013198ebb222d5ddc43b073ecb36462cb9
SanekWhite <bro.b98@mail.ru>
2024-09-30 21:40:03 +0700

    Initial Commit

---
+++ AutoPark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoPark
+//
+//  Created by Александр Белый on 30.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ newAutoPark.swift
@@ -0,0 +1,8 @@
+//
+//  newAutoPark.swift
+//  AutoPark
+//
+//  Created by Александр Белый on 30.09.2024.
+//
+
+import Foundation
